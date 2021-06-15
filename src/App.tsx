377ae196ddb19397b93ecc59954678becb1de707
BarrosAmorim <barrosamorimd@gmail.com>
2021-06-15 11:53:29 -0300

    Configurando modal de criação

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,27 @@
+import { useState } from 'react'
+import Modal from 'react-modal'
 import { Dashboard } from './components/Dashboard'
 import { Header } from './components/Header'
 import { GlobalStyle } from './styles/global'
 
+Modal.setAppElement('#root')
+
 export const App = () => {
+  const [modalIsOpen,setModalIsOpen] = useState(false)
+
+    const hadleOpenModal = () => {
+        setModalIsOpen(true)
+    }
+    const hadleCloseModal = () => {
+        setModalIsOpen(false)
+    }
   return (
     <>
-      <Header />
+      <Header onOpenNewModal={hadleOpenModal} />
       <Dashboard />
+      <Modal isOpen={modalIsOpen} onRequestClose={hadleCloseModal} >
+        <h2>Cadastrar</h2>
+      </Modal>
       <GlobalStyle />
     </>
   )
--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,13 +1,17 @@
 import logoImg from '../../assets/logo.svg'
 import { Container, Content } from './styles'
 
-export const Header = () => {
+type HeaderProps = {
+    onOpenNewModal: () => void
+}
+
+export const Header = ({ onOpenNewModal }: HeaderProps) => {
     return (
         <div>
             <Container>
                 <Content>
                     <img src={logoImg} alt="dt money" />
-                    <button type="button">Nova Transação</button>
+                    <button type="button" onClick={onOpenNewModal}>Nova Transação</button>
                 </Content>
             </Container>
         </div>
